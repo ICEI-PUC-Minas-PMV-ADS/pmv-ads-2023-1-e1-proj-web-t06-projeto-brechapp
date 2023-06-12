9b737e24ec1ac3bb2068b23cb5f4bfac1156de83
Carolina Mascarenhas <125296093+CarolMGC@users.noreply.github.com>
2023-06-11 23:15:22 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
-function logalt(){
-  
+let navegacao = document.getElementById('TxtBusca')
+function buscar(){
+  if(navegacao >2){
+    navegacao.setAttribute(localStorage.getItem(nameprodut))
+    const result = nameprodut.filter(nameprodut => nameprodut.lenght > 2)
+
+  }
+
 }
